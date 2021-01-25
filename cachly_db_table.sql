ec09bbc232f0dd25839e7dd0834d9cebaf614a80
Gen Taliaru <gen@infira.ee>
2021-01-25 22:20:39 +0200

    db schema change

--- cachly_db_table.sql
+++ cachly_db_table.sql
@@ -2,14 +2,10 @@
 -- Table structure for table `cachly_cache`
 -- Cause Cachly default hashing algorithm is sh1 then ID is CHAR(40)
 --
-
 CREATE TABLE `cachly_cache` (
-  `ID` char(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `data` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `insertDate` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
-  `expires` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'timestamp when will be expired'
+	`ID` char(40) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	`data` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+	`insertDate` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+	`expires` datetime DEFAULT NULL COMMENT 'timestamp when will be expired'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE `cachly_cache`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `expires` (`expires`);
-COMMIT;
+ALTER TABLE `cachly_cache` ADD PRIMARY KEY (`ID`),ADD KEY `expires` (`expires`);
